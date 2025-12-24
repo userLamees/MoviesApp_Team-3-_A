efc88604e989be7ffe1e29974ef87d054241b175
Lamees <lamees1harbi@gmail.com>
2025-12-24 14:35:57 +0300

    Add files via upload

---
+++ MoviesApp/MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by lamess on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoviesApp/MoviesAppTests/MoviesAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesAppTests.swift
+//  MoviesAppTests
+//
+//  Created by lamess on 24/12/2025.
+//
+
+import Testing
+
+struct MoviesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
