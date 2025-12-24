3cfcf5f9bbf5bce3e21bd8d70e250fb65df8b486
Lamees <lamees1harbi@gmail.com>
2025-12-24 15:05:11 +0300

    added sign in screen

---
+++ MoviesApp/MoviesApp/Views/SigninView.swift
@@ -0,0 +1,18 @@
+//
+//  SigninView.swift
+//  MoviesApp
+//
+//  Created by lamess on 24/12/2025.
+//
+
+import SwiftUI
+
+struct SigninView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SigninView()
+}
